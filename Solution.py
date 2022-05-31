0f300b7e60a6a941396ae442eaf7133fb779360b
Adel Khatib <adelkhatib13@gmail.com>
2022-05-31 20:13:32 +0300

    getFilesCanBeAddedToDiskAndRAM

--- Solution.py
+++ Solution.py
@@ -652,7 +652,33 @@ def getFilesCanBeAddedToDiskAndRAM(diskID: int) -> List[int]:
     # where size <= freespace of this disk (sub-query)
     # where size <= sum of... (same sub-query used in the func "diskTotalRAM")
     # LIMIT 5
-    return []
+    conn = None
+    try:
+        conn = Connector.DBConnector()
+        query = sql.SQL(
+            "SELECT file_id "
+            "FROM files "
+            "WHERE size <= (SELECT free_space FROM disks WHERE disk_id = {dID})"
+            "WHERE size <= ("
+            "SELECT COALESCE(SUM(size),0) as size_sum"
+            "FROM disks_ram_enhanced_ram_details "
+            "WHERE disk_id={dID}"
+            ")"
+            "ORDER BY file_id ASC"
+            "LIMIT 5"
+        ).format(
+            dID=sql.Literal(diskID)
+        )
+        _, result = conn.execute(query)
+        conn.commit()
+    except DatabaseException as e:
+        return []
+    finally:
+        # will happen any way after try termination or exception handling
+        conn.close()
+    # need to convert (check the output) (maybe should concatenate result.rows)
+    # something like return [next(iter(row)) for row in result.rows]
+    return result
 
 
 def isCompanyExclusive(diskID: int) -> bool:
